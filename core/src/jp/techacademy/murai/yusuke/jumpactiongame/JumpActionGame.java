5af4a229d5d1f4d3a426ddf7d5124e9adfd16e11
ym.devtech <ym.devtech@gmail.com>
2017-01-25 14:42:28 +0900

    Initial commit

---
+++ core/src/jp/techacademy/murai/yusuke/jumpactiongame/JumpActionGame.java
@@ -0,0 +1,50 @@
+package jp.techacademy.murai.yusuke.jumpactiongame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class JumpActionGame extends Game {
+
+
+	// publicにして外からアクセスできるようにする
+	public SpriteBatch batch;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+
+		// GameScreenを表示する
+		setScreen(new GameScreen(this));
+	}
+
+
+
+	/*	SpriteBatch batch;
+	Texture img;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}*/
+
+}
