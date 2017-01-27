84bd261822854bcb0d99b036800632ed3e65cbe8
ym.devtech <ym.devtech@gmail.com>
2017-01-28 01:13:42 +0900

    ５まで実装

--- core/src/jp/techacademy/murai/yusuke/jumpactiongame/GameScreen.java
+++ core/src/jp/techacademy/murai/yusuke/jumpactiongame/GameScreen.java
@@ -3,18 +3,26 @@ package jp.techacademy.murai.yusuke.jumpactiongame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
 /**
  * Created by my99workmac on 2017/01/26.
  */
 
 public class GameScreen extends ScreenAdapter {
 
+    static final float CAMERA_WIDTH = 10;
+    static final float CAMERA_HEIGHT = 15;
+
     private JumpActionGame mGame;
 
     Sprite mBg;
+    OrthographicCamera mCamera;
+    FitViewport mViewPort;
 
     public GameScreen(JumpActionGame game) {
         mGame = game;
@@ -23,7 +31,13 @@ public class GameScreen extends ScreenAdapter {
         Texture bgTexture = new Texture("back.png");
         // TextureRegionで切り出す時の原点は左上
         mBg = new Sprite( new TextureRegion(bgTexture, 0, 0, 540, 810));
+        mBg.setSize(CAMERA_WIDTH, CAMERA_HEIGHT);
         mBg.setPosition(0, 0);
+
+        // カメラ、ViewPortを生成、設定する
+        mCamera = new OrthographicCamera();
+        mCamera.setToOrtho(false, CAMERA_WIDTH, CAMERA_HEIGHT);
+        mViewPort = new FitViewport(CAMERA_WIDTH, CAMERA_HEIGHT, mCamera);
     }
 
     @Override
@@ -31,8 +45,21 @@ public class GameScreen extends ScreenAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // カメラの座標をアップデート（計算）し、スプライトの表示に反映させる
+        mCamera.update();
+        mGame.batch.setProjectionMatrix(mCamera.combined);
+
         mGame.batch.begin();
+
+        // 原点は左下
+        mBg.setPosition(mCamera.position.x - CAMERA_WIDTH / 2, mCamera.position.y - CAMERA_HEIGHT / 2);
         mBg.draw(mGame.batch);
+
         mGame.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        mViewPort.update(width, height);
     }
 }
