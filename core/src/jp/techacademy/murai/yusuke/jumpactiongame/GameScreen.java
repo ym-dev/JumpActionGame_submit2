d257a99311e53c9a7fca66945a9f230e6073e2ed
ym.devtech <ym.devtech@gmail.com>
2017-01-29 19:10:33 +0900

    ６まで実装

--- core/src/jp/techacademy/murai/yusuke/jumpactiongame/GameScreen.java
+++ core/src/jp/techacademy/murai/yusuke/jumpactiongame/GameScreen.java
@@ -9,6 +9,10 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by my99workmac on 2017/01/26.
  */
@@ -17,12 +21,30 @@ public class GameScreen extends ScreenAdapter {
 
     static final float CAMERA_WIDTH = 10;
     static final float CAMERA_HEIGHT = 15;
+    static final float WORLD_WIDTH = 10;
+    static final float WORLD_HEIGHT = 15 * 10; // 20画面分登れば終了
+
+    static final int GAME_STATE_READY = 0;
+    static final int GAME_STATE_PLAYING = 1;
+    static final int GAME_STATE_GAMEOVER = 2;
+
+    // 重力
+    static final float GRAVITY = -12;
+
 
     private JumpActionGame mGame;
 
     Sprite mBg;
     OrthographicCamera mCamera;
     FitViewport mViewPort;
+
+    Random mRandom;
+    List<Step> mSteps;
+    List<Star> mStars;
+    Ufo mUfo;
+    Player mPlayer;
+
+    int mGameState;
 
     public GameScreen(JumpActionGame game) {
         mGame = game;
@@ -38,6 +60,14 @@ public class GameScreen extends ScreenAdapter {
         mCamera = new OrthographicCamera();
         mCamera.setToOrtho(false, CAMERA_WIDTH, CAMERA_HEIGHT);
         mViewPort = new FitViewport(CAMERA_WIDTH, CAMERA_HEIGHT, mCamera);
+
+        // メンバ変数の初期化
+        mRandom = new Random();
+        mSteps = new ArrayList<Step>();
+        mStars = new ArrayList<Star>();
+        mGameState = GAME_STATE_READY;
+
+        createStage();
     }
 
     @Override
@@ -45,15 +75,35 @@ public class GameScreen extends ScreenAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // それぞれの状態をアップデートする
+        update(delta);
+
         // カメラの座標をアップデート（計算）し、スプライトの表示に反映させる
         mCamera.update();
         mGame.batch.setProjectionMatrix(mCamera.combined);
 
         mGame.batch.begin();
 
+        //背景
         // 原点は左下
         mBg.setPosition(mCamera.position.x - CAMERA_WIDTH / 2, mCamera.position.y - CAMERA_HEIGHT / 2);
         mBg.draw(mGame.batch);
+
+        // Step
+        for (int i = 0; i < mSteps.size(); i++) {
+            mSteps.get(i).draw(mGame.batch);
+        }
+
+        // Star
+        for (int i = 0; i < mStars.size(); i++) {
+            mStars.get(i).draw(mGame.batch);
+        }
+
+        // UFO
+        mUfo.draw(mGame.batch);
+
+        //Player
+        mPlayer.draw(mGame.batch);
 
         mGame.batch.end();
     }
@@ -61,5 +111,58 @@ public class GameScreen extends ScreenAdapter {
     @Override
     public void resize(int width, int height) {
         mViewPort.update(width, height);
+    }
+
+
+    // ステージを作成する
+    private void createStage() {
+
+        // テクスチャの準備
+        Texture stepTexture = new Texture("step.png");
+        Texture starTexture = new Texture("star.png");
+        Texture playerTexture = new Texture("uma.png");
+        Texture ufoTexture = new Texture("ufo.png");
+
+        // StepとStarをゴールの高さまで配置していく
+        float y = 0;
+
+        float maxJumpHeight = Player.PLAYER_JUMP_VELOCITY * Player.PLAYER_JUMP_VELOCITY / (2 * -GRAVITY);   //h = 1/2*m*v*v
+        while (y < WORLD_HEIGHT - 5) {
+            int type = mRandom.nextFloat() > 0.8f ? Step.STEP_TYPE_MOVING : Step.STEP_TYPE_STATIC;
+            float x = mRandom.nextFloat() * (WORLD_WIDTH - Step.STEP_WIDTH);
+
+            Step step = new Step(type, stepTexture, 0, 0, 144, 36);
+            step.setPosition(x, y);
+            mSteps.add(step);
+
+            if (mRandom.nextFloat() > 0.6f) {
+                Star star = new Star(starTexture, 0, 0, 72, 72);
+                star.setPosition(step.getX() + mRandom.nextFloat(), step.getY() + Star.STAR_HEIGHT + mRandom.nextFloat() * 3);
+                mStars.add(star);
+            }
+
+            y += (maxJumpHeight - 0.5f);
+            y -= mRandom.nextFloat() * (maxJumpHeight / 3);
+        }
+
+        // Playerを配置
+        mPlayer = new Player(playerTexture, 0, 0, 72, 72);
+        mPlayer.setPosition(WORLD_WIDTH / 2 - mPlayer.getWidth() / 2, Step.STEP_HEIGHT);
+
+        // ゴールのUFOを配置
+        mUfo = new Ufo(ufoTexture, 0, 0, 120, 74);
+        mUfo.setPosition(WORLD_WIDTH / 2 - Ufo.UFO_WIDTH / 2, y);
+    }
+
+    // それぞれのオブジェクトの状態をアップデートする
+    private void update(float delta) {
+        switch (mGameState) {
+            case GAME_STATE_READY:
+                break;
+            case GAME_STATE_PLAYING:
+                break;
+            case GAME_STATE_GAMEOVER:
+                break;
+        }
     }
 }
