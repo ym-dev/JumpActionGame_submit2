ec90ffb8676044cb6457a0bbf49782a8ff707aee
ym-dev <ym.devtech@gmail.com>
2017-02-03 21:48:52 +0900

    AndroidLauncheにActivityRequestHandler追加

---
+++ core/src/jp/techacademy/murai/yusuke/jumpactiongame/ActivityRequestHandler.java
@@ -0,0 +1,9 @@
+package jp.techacademy.murai.yusuke.jumpactiongame;
+
+/**
+ * Created by oamac on 2017/02/03.
+ */
+
+public interface ActivityRequestHandler {
+    public void showAds(boolean show);
+}
